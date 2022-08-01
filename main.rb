029eb0ca6c34b67dda2f7e378aa8bc41f4e8389f
Mustafa <971530+tosbaha@users.noreply.github.com>
2022-08-01 11:36:30 +0300

    Save log file to artifacts

--- main.rb
+++ main.rb
@@ -1,3 +1,9 @@
+require 'pathname'
+
+def get_env_variable(key)
+	return (ENV[key] == nil || ENV[key] == "") ? nil : ENV[key]
+end
+
 def run_command(command)
   puts "@@[command] #{command}"
   unless system(command)
@@ -6,10 +12,12 @@ def run_command(command)
 end
 
 cli_version = ENV['AC_APPIUM_VERSION']
+ac_output_path = get_env_variable("AC_OUTPUT_DIR") || abort('Missing output path.')
+appium_log_file = (Pathname.new ac_output_path).join("appium.log")
 appium_version = ''
 appium_version = "@#{cli_version}" if !cli_version.nil? && !cli_version.empty?
 
 install_appium = "npm install -g appium#{appium_version} --unsafe-perm=true --allow-root"
-run_appium = "appium --log appium.log &>/dev/null &"
+run_appium = "appium --log #{appium_log_file} &>/dev/null &"
 run_command(install_appium)
 run_command(run_appium)
